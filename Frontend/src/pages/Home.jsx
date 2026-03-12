43381152c9410a8940e561a2bd7b5ccd6c3cb258
Emirhan <dev.emirhangundogdu@gmail.com>
2026-03-12 16:04:42 +0300

    Create Frontend

---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Hero from '../components/Hero';
+import ProductList from '../components/ProductList';
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <ProductList />
+    </>
+  );
+};
+
+export default Home;
